24f668bbe6c0474b426bdbf0d8a3b9dbb0a16aaf
mohamedeslam577 <133720724+mohamedeslam577@users.noreply.github.com>
2026-06-17 21:51:08 +0300

    Add files via upload

---
+++ test_db.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import psycopg2
+import pandas as pd
+
+app = FastAPI()
+
+DATABASE_URL = "postgresql://postgres:YqavFXcTXlyBuMNKTWObIkZeiGodEuiA@kodama.proxy.rlwy.net:26773/railway"
+
+@app.get("/recommendation-data")
+def get_data():
+
+    conn = psycopg2.connect(DATABASE_URL)
+
+    places = pd.read_sql(
+        "SELECT * FROM recommendation_places",
+        conn
+    )
+
+    children = pd.read_sql(
+        "SELECT * FROM child_places",
+        conn
+    )
+
+    conn.close()
+
+    return {
+        "recommendation_places": places.to_dict(orient="records"),
+        "child_places": children.to_dict(orient="records")
+    }
